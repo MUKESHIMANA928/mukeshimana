b5d90fa5ef13f28d35833ef04f73518abbcf2c9a
MUKESHIMANA928 <150811329+MUKESHIMANA928@users.noreply.github.com>
2023-11-17 12:08:20 +0200

    Add files via upload

---
+++ work/do.java
@@ -0,0 +1,12 @@
+class GFG {
+    public static void main (String[] args) {
+      int i=1;
+      do
+        
+        System.out.println("Hello GFG!");
+      
+      while(i>=3);
+       
+       
+    }
+}
---
+++ work/for2.java
@@ -0,0 +1,12 @@
+
+
+class Main {
+  public static void main(String[] args) {
+      
+    int[] numbers = {3, 7, 5, -5};
+    
+    for (int number: numbers) {
+       System.out.println(number);
+    }
+  }
+}
